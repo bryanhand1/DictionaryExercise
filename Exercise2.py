99e9bd43634deb2ab33e4109457d1c086469cf7b
Bryan Hand <bryan_hand1@baylor.edu>
2021-02-05 15:44:16 -0600

    Revised Submission

--- Exercise2.py
+++ Exercise2.py
@@ -10,7 +10,18 @@ winners = []
 for w in WSwinners:
     winners.append(w.rstrip("\n" + "\r"))
 
+num_wins = {}
 
+
+### Create a dictionary with teams and times won
+for team in winners:
+    if team in num_wins.keys():
+        num_wins[team] += 1
+    else:
+        num_wins[team] = 1
+
+
+### Create a dictionary with years and winners
 ### Create a list of years from 1903-2008 without 1904 and 1994
 
 years = [1903]
@@ -20,7 +31,7 @@ for y in range(1905, 1994):
     years.append(y)
 
 # add second set of years to list
-for y in range(1995, 2008):
+for y in range(1995, 2009):
     years.append(y)
 
 # create a dictionary with key and value from both list
@@ -31,11 +42,15 @@ WSdict = {years[i]: winners[i] for i in range(len(years))}
 given_year = int(input("What year would you like to see the World Series Winner? "))
 
 if given_year in WSdict.keys():
+    team = WSdict[given_year]
     print(
         "The World Series Winner in "
         + str(given_year)
         + " was the "
         + WSdict[given_year]
+    )
+    print(
+        "The " + team + " has won the World Series " + str(num_wins[team]) + " times."
     )
 else:
     print(
